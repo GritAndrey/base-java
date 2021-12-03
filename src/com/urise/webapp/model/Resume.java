15aa70aa1de07d298a5bd2b26725ecd328ce6968
Gritsunick Andrey <stendarr@gmail.com>
2021-12-03 16:21:18 +0300

    added equals and hashcode

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -8,6 +8,21 @@ public class Resume {
     // Unique identifier
     private String uuid;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Resume resume = (Resume) o;
+
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
+
     public String getUuid() {
         return uuid;
     }
