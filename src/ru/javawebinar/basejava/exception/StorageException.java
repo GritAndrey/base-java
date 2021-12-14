f802170de0f658230a27020637d0cd62edf32308
Gritsunick Andrey <stendarr@gmail.com>
2021-12-14 12:02:05 +0300

    * AbstractFileStorage crud

--- src/ru/javawebinar/basejava/exception/StorageException.java
+++ src/ru/javawebinar/basejava/exception/StorageException.java
@@ -1,10 +1,17 @@
 package ru.javawebinar.basejava.exception;
 
+import java.io.IOException;
+
 public class StorageException extends RuntimeException {
     private final String uuid;
 
     public StorageException(String msg, String uuid) {
         super(msg + uuid);
+        this.uuid = uuid;
+    }
+
+    public StorageException(String msg, String uuid, IOException e) {
+        super(msg + uuid, e);
         this.uuid = uuid;
     }
 
--- src/ru/javawebinar/basejava/storage/AbstractFileStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractFileStorage.java
@@ -1,7 +1,13 @@
 package ru.javawebinar.basejava.storage;
 
+import ru.javawebinar.basejava.exception.StorageException;
+import ru.javawebinar.basejava.model.Resume;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 public abstract class AbstractFileStorage extends AbstractStorage<File> {
     private final File storageDir;
@@ -18,12 +24,77 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
     }
 
     @Override
+    public void clear() {
+        Arrays.stream(getNotNullFilesList()).forEach(this::makeDelete);
+    }
+
+    @Override
+    public int size() {
+        String[] list = storageDir.list();
+        return list == null ? 0 : list.length;
+    }
+
+    @Override
     protected boolean isExist(File key) {
         return key.exists();
     }
 
     @Override
     protected File getKey(String uuid) {
-        return new File(storageDir,uuid);
+        return new File(storageDir, uuid);
     }
+
+    @Override
+    protected void makeUpdate(File key, Resume resume) {
+        try {
+            write(key, resume);
+        } catch (IOException e) {
+            throw new StorageException("File IOError", key.toString(), e);
+        }
+    }
+
+    @Override
+    protected void makeSave(File key, Resume resume) {
+        try {
+            boolean result = key.createNewFile();
+            if (!result) {
+                throw new Error("Impossible");
+            }
+        } catch (IOException e) {
+            throw new StorageException("File IOError", key.toString(), e);
+        }
+        makeUpdate(key, resume);
+    }
+
+    @Override
+    protected void makeDelete(File key) {
+        if (!key.delete()) {
+            throw new StorageException("Can`t delete file", key.toString());
+        }
+    }
+
+    @Override
+    protected Resume makeGet(File key) {
+        try {
+            return read(key);
+        } catch (IOException e) {
+            throw new StorageException("File IOError", key.toString(), e);
+        }
+    }
+
+    @Override
+    protected Stream<Resume> getStorageStream() {
+        return Arrays.stream(getNotNullFilesList()).map(this::makeGet);
+
+    }
+
+    private File[] getNotNullFilesList() {
+        if (storageDir.listFiles() != null)
+            return storageDir.listFiles();
+        throw new NullPointerException();
+    }
+
+    protected abstract Resume read(File key) throws IOException;
+
+    protected abstract void write(File key, Resume resume) throws IOException;
 }
