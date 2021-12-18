638bd43d71561b8c1e4dd3c9eba289f60526418e
Gritsunick Andrey <stendarr@gmail.com>
2021-12-18 17:42:24 +0300

    * XMLParse util

---
+++ src/ru/javawebinar/basejava/util/XMLParser.java
@@ -0,0 +1,46 @@
+package ru.javawebinar.basejava.util;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+public class XMLParser {
+    private final Marshaller marshaller;
+    private final Unmarshaller unmarshaller;
+
+    @SuppressWarnings("rawtypes")
+    public XMLParser(Class... classesToBeBound) {
+        try {
+            JAXBContext ctx = JAXBContext.newInstance(classesToBeBound);
+
+            marshaller = ctx.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_ENCODING, StandardCharsets.UTF_8);
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            //marshaller.setProperty(Marshaller.JAXB_FRAGMENT,true);
+            unmarshaller = ctx.createUnmarshaller();
+        } catch (JAXBException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T unmarshall(Reader reader) {
+        try {
+            return (T) unmarshaller.unmarshal(reader);
+        } catch (JAXBException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public void marshall(Object instance, Writer writer) {
+        try {
+            marshaller.marshal(instance, writer);
+        } catch (JAXBException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
