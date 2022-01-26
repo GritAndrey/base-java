e4e05674b8353c5fbf99bb348093e28859facd0c
Gritsunick Andrey <stendarr@gmail.com>
2022-01-26 14:42:42 +0300

    * LazySingleton

---
+++ src/ru/javawebinar/basejava/LazySingleton.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.basejava;
+
+public class LazySingleton {
+    volatile private static LazySingleton INSTANCE;
+
+    private LazySingleton() {
+    }
+
+    public static LazySingleton getInstance() {
+        return LazySingletonHolder.INSTANCE;
+//        if (INSTANCE == null) {
+//            synchronized (LazySingleton.class) {
+//                if (INSTANCE == null) {
+//                    INSTANCE = new LazySingleton();
+//                }
+//            }
+//        }
+//        return INSTANCE;
+    }
+
+    private static class LazySingletonHolder {
+        private static final LazySingleton INSTANCE = new LazySingleton();
+    }
+}
+
