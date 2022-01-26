2fc1a96829fd9bb6740725c626f6ca706609eb45
Gritsunick Andrey <stendarr@gmail.com>
2022-01-26 14:40:30 +0300

    * webinar work

--- src/ru/javawebinar/basejava/util/MainConcurrency.java
+++ src/ru/javawebinar/basejava/util/MainConcurrency.java
@@ -1,14 +1,49 @@
 package ru.javawebinar.basejava.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainConcurrency {
-    public static void main(String[] args) {
+    private static final Object LOCK = new Object();
+    public static final int THREADS_NUMBER = 10000;
+    private static int counter;
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
-        new Thread() {
+        final Thread thread0 = new Thread() {
             @Override
             public void run() {
-                System.out.println(getName());
+                System.out.println(getName() + ", " + getState());
+                throw new IllegalStateException();
             }
-        }.start();
-        new Thread(() -> System.out.println(Thread.currentThread().getName())).start();
+        };
+        thread0.start();
+        new Thread(() -> System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState())).start();
+
+        System.out.println(thread0.getState());
+
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        final List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            final Thread thread = new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                }
+            });
+            thread.start();
+            threads.add(thread);
+        }
+        threads.forEach(thr-> {
+            try {
+                thr.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(counter);
+    }
+
+    private synchronized void inc() {
+            counter++;
     }
 }
