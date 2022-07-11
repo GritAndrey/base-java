3b3c26e434f764b5f2b6ba330bbfed85c170cac7
Gritsunick Andrey <stendarr@gmail.com>
2022-07-11 16:57:40 +0300

    Added SqlStorage

--- src/ru/javawebinar/basejava/exception/StorageException.java
+++ src/ru/javawebinar/basejava/exception/StorageException.java
@@ -12,6 +12,10 @@ public class StorageException extends RuntimeException {
         this.uuid = uuid;
     }
 
+    public StorageException(Exception e) {
+        this(e.getMessage(), e);
+    }
+
     public StorageException(String msg, Exception e) {
         this(msg, null, e);
     }
