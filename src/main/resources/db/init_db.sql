8e45c6d63fc7fbf4e8621f34487ea550cc000d48
Gritsunick Andrey <stendarr@gmail.com>
2022-07-11 14:37:48 +0300

    Init DB

---
+++ src/main/resources/db/init_db.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS contact;
+DROP TABLE IF EXISTS resume;
+CREATE TABLE resume
+(
+    uuid      CHAR(36) PRIMARY KEY NOT NULL,
+    full_name TEXT                 NOT NULL
+);
+
+CREATE TABLE contact
+(
+    id          SERIAL,
+    resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+    type        TEXT     NOT NULL,
+    value       TEXT     NOT NULL
+);
+CREATE UNIQUE INDEX contact_uuid_type_index
+    ON contact (resume_uuid, type);
